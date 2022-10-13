27c0ae6203d154a5cd0f2e77ac024eca340a19e6
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-12 21:52:55 -0300

    Create funca0.js

---
+++ JavaScriptCursoemVideo/aulafunçoes/funca0.js
@@ -0,0 +1,12 @@
+
+function parimp(n){
+    if (n % 2 == 0){        
+        return 'par'
+    }else{             
+        return 'impar'
+    }
+    
+
+}
+let valor = parimp(10)
+console.log(valor)
