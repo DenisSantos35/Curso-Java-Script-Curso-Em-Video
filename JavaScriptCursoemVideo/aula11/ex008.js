e0462f157600e0e998bb641e8548aae6ac9d2383
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-12 21:48:24 -0300

    Create ex008.js

---
+++ JavaScriptCursoemVideo/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50.8
+console.log(`A velocidade do meu carro e ${vel}Km/h`)
+if( vel > 60){ //condiçao simples
+    console.log(`Voce ultrapassou a velocidade permitida multado`)
+}
+console.log(`Dirija sempre utilizando sinto de segurança`)
