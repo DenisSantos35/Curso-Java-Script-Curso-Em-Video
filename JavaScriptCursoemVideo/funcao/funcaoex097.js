dce947a4decb5f8686fbb2b5e6c66580e94a214a
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-12 21:39:23 -0300

    Create funcaoex097.js

---
+++ JavaScriptCursoemVideo/funcao/funcaoex097.js
@@ -0,0 +1,10 @@
+function escreva(msg){
+    console.log(msg.length *) 
+    console.log(msg)
+    console.log(msg.length)
+}
+
+
+escreva('  GUSTAVO GUANABARA  ')
+escreva('   CURSO JAVA SCRIPT NO YOU TUBE  ')
+escreva('  CURSO JAVA SCRIPT NO CURSO EM VIDEO  ')
