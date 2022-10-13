a8c078e6096a5984604dcf7cb3dcad506c662501
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-12 21:39:47 -0300

    Create objeto01.js

---
+++ JavaScriptCursoemVideo/objeto/objeto01.js
@@ -0,0 +1,19 @@
+/*let amigo = {nome:'jose',
+sexo: 'm',
+peso: 85.4,
+engordar(p=0){
+    console.log('Engordou')
+    this.peso += p
+}}
+amigo.engordar(5) 
+console.log(`${amigo.nome} pesa ${amigo.peso}Kg`)
+let num = 8
+console.log(num**=2)*/
+jogador = {}
+gol = []
+dados = []
+gol.push(2,4)
+console.log(gol)
+jogador['gol'] = gol
+dados.push(jogador)
+console.log(dados)
