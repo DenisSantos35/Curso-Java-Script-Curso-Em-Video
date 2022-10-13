33df38c0ee9ee7a103a0978738108ea5c2d2ea53
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-12 21:39:36 -0300

    Create funcaopyemjs.js

---
+++ JavaScriptCursoemVideo/funcao/funcaopyemjs.js
@@ -0,0 +1,17 @@
+function txt(msg){
+    console.log(msg)
+    console.log('---------------------')
+}
+
+function area(l,c){
+    let ar = l * c
+    console.log(`A area de um terreno ${l} X ${c} é de ${ar}`)
+}
+
+txt('CONTRODE DE TERRENO')
+let larg = 4.5
+let compr = 8.0
+
+area(larg,compr)
+
+
