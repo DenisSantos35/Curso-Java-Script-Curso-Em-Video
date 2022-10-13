8b7c42d2dc3bfbde2e78779a03991eb0716bd661
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-12 21:51:42 -0300

    Create ex05arrays.js

---
+++ JavaScriptCursoemVideo/aula15arrays/ex05arrays.js
@@ -0,0 +1,24 @@
+/*let vetor = []
+let min = 1
+let max = 10
+function getRandomInt(max,min){    
+    min = Math.ceil(min)
+    max = Math.floor(max)
+
+    return Math.floor(Math.random() * (max -min)+min)
+}
+console.log(getRandomInt(max,min))
+
+for(let i = 0; i < 7; i++){
+    vetor[i] = getRandomInt(max,min)
+}
+for(let i = 0; i < 7; i++){
+    console.log(vetor[i]) 
+}*/
+numero = [1,2,3,4]
+numero[2] = 7
+console.log(numero)
+numero.push(5)
+console.log(numero)
+numero.sort()
+console.log(numero)
