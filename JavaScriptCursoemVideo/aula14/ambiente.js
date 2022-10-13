2379f974883cfb881ccf0ba90b319795f842ca46
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-12 21:50:38 -0300

    Create ambiente.js

---
+++ JavaScriptCursoemVideo/aula14/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+    do{
+    console.log('tudo Bem?')
+    console.log(`Passo ${c}`)
+    c++}    while( c <= 2)
+/*
+var c = 1
+
+while( c <= 5){
+    console.log('tudo Bem?')
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+/*tudo bem
+console.log('tudo Bem?')
+console.log('tudo Bem?')
+console.log('tudo Bem?')
+console.log('tudo Bem?')
+console.log('tudo Bem?')
+console.log('tudo Bem?')
+*/
