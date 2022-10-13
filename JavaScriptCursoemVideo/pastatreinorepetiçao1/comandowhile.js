43829dda6262ea6cf496759ecf239b2a4f2747ec
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-12 22:13:26 -0300

    Create comandowhile.js

---
+++ JavaScriptCursoemVideo/pastatreinorepetiçao1/comandowhile.js
@@ -0,0 +1,29 @@
+var cont = 0
+var vezes = 9
+while(vezes > 2){
+    vezes--
+    cont++
+}
+console.log(`O loop executou ${cont}`)
+
+var i = 1
+var result = 0
+while (i <= 10){
+    result = result + 1
+    console.log(`O valor do resultado = ${result}`)
+    i++
+}
+var cont2 = 0
+do{
+    vezes = vezes - 1
+    cont2++
+}while(vezes > 10)
+console.log(vezes)
+
+var z = 0
+
+do{
+    z = z+1
+}while(z < 10)
+console.log(`Valor ${z}`)
+
